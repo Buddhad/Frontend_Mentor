0180e3560bb659674359637558a3f3fa010a44e3
Buddhadeb Chhetri <buddhadebchhetri6@gmail.com>
2023-03-24 10:00:13 +0530

    Added functionality and cursor

--- Challenge_08/script.js
+++ Challenge_08/script.js
@@ -32,7 +32,8 @@ const quizData =[
         correct: 'd',
     }
 ]
-
+const quiz= document.getElementById("quiz");
+const answerEls=document.querySelectorAll(".answer");
 const questionEl =document.getElementById('question');
 const a_text =document.getElementById('a_text');
 const b_text =document.getElementById('b_text');
@@ -41,30 +42,56 @@ const d_text =document.getElementById('d_text');
 const submitBtn =document.getElementById("btn");
 
 let currentQuiz=0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz(){
+    deselectAnswers();
     const currentQuizData=quizData[currentQuiz];
-
     questionEl.innerText=currentQuizData.question;
-
     a_text.innerText= currentQuizData.a;
     b_text.innerText= currentQuizData.b;
     c_text.innerText= currentQuizData.c;
     d_text.innerText= currentQuizData.d;
+}
 
 
+function getSelected(){
+    let answer=undefined;
+    answerEls.forEach((answerEl)=>{
+    if(answerEl.checked){
+        answer =answerEl.id; 
+    }
+    });
+    return answer;
+}
+
+function deselectAnswers(){
+    answerEls.forEach((answerEl)=> {
+        answerEl.checked=false;
+    });
 }
 
 submitBtn.addEventListener("click" ,()=>{
-    currentQuiz++;
-    if(currentQuiz<quizData.length){
-        loadQuiz();
-    }
-    else{
-        alert("You completed the quiz!!");
-    }
+//Check to see the answer
+    const answer = getSelected();
     
+    if(answer){
+        
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+            
+        }
+        currentQuiz++;
+
+        //Show the Results
+        if(currentQuiz<quizData.length){ 
+            loadQuiz();
+        }
+        else{
+            quiz.innerHTML=`<h2>You have answer Correctly at ${score}/${quizData.length} questions.</h2> <button onclick="location.reload();">Reload</button>`;
+        }
+    }
 }
-)
+);
